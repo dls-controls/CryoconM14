23ead27547435d0dac80b2cbc3bb1db13636f04e
Keith Baker <Keith.Baker@diamond.ac.uk>
2008-09-19 14:05:18 +0000

    First import of support for the Cryocon M14 4-channel temperature monitor

---
+++ example/exampleApp/opi/runGUI.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. /home/tools/bin/changeports 6064
+#EDMDATAFILES=.:/dls_sw/work/R3.14.8.2/support/CryoconM14/data
+EDMDATAFILES=.:../../../CryoconM14App/opi/edl
+export EDMDATAFILES
+edm -x -eolc -m "device=TS-ID-TMON-01" M14.edl &
+
