e2fe185a2953837f6a5cfa9d90e4f6045be7cedf
Keith Baker <Keith.Baker@diamond.ac.uk>
2008-10-08 13:19:17 +0000

    Corrected differentiation between the device and the temperature records

--- example/exampleApp/opi/runGUI.sh
+++ example/exampleApp/opi/runGUI.sh
@@ -3,5 +3,5 @@
 EDMDATAFILES=.:/dls_sw/work/R3.14.8.2/support/CryoconM14/data
 #EDMDATAFILES=.:../../../CryoconM14App/opi/edl
 export EDMDATAFILES
-edm -x -eolc -m "device=TS-ID-TMON-01,record1=T1,record2=T2,record3=T3,record4=T4" M14.edl &
+edm -x -eolc -m "tmon=TS-ID-TMON-01,device=TS-ID-TMON-01,record1=T1,record2=T2,record3=T3,record4=T4" M14.edl &
 
